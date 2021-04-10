0f68bc638fa83c74dec7237261dfc1be554b0a25
nenas1ya <okolotec@gmail.com>
2021-04-10 12:20:23 +0500

    hw-3 chat base

--- hw-4 . chat/index.js
+++ hw-4 . chat/index.js
@@ -2,11 +2,22 @@ const express = require('express')
 const app = express()
 const http = require('http')
 const server = http.createServer(app)
+const io = require('socket.io')(server)
 
 app.get('/', (req, res) => {
-  res.send('<h1>hello world</h1>')
+  res.sendFile(__dirname + '/index.html')
 })
 
-server.listen(3000, () => {
-  console.log('listening on *:3000')
+io.on('connection', (socket) => {
+  socket.broadcast.emit('hi')
+  socket.on('disconnect', () => {
+    console.log('user disconnected')
+  })
+  socket.on('chat message', (msg) => {
+    io.emit('chat message', msg)
+  })
+})
+
+server.listen(1337, () => {
+  console.log('listening on *:1337')
 })
